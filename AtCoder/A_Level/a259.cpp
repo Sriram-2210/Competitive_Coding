d54dd1b45563703e07d79bdcc9b55c62625d8852
Sriram <65888303+Sriram-2210@users.noreply.github.com>
2022-08-27 06:10:19 +0000

    Commit

--- AtCoder/A_Level/a259.cpp
+++ AtCoder/A_Level/a259.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n,x,m,t,d;
+    cin >> n >> m >> x >> t >> d;
+    if(m>=x) cout << t;
+    else cout << t-x*d+m;
     return 0;
 }
